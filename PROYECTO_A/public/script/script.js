910ca1aefba3524ce73cf75c5d60ee47807d3c55
marsarasola <sarasolamartina21@gmail.com>
2023-06-20 22:08:46 -0300

    muchos cambios

--- PROYECTO_A/public/script/script.js
+++ PROYECTO_A/public/script/script.js
@@ -4,8 +4,15 @@ const btn = document.querySelector('#counter-btn')
 
 const addOneBoludo = () => {
     amountOfBoludos++;
-    let quote = `Agregue ${amountOfBoludos}`;
+    let quote = `${amountOfBoludos}`;
     result.textContent = quote;
+    var resultado = document.getElementById('result');
+    resultado.style.setProperty("background-color","#000000");
+    resultado.style.setProperty("color","#ffffff");
+    resultado.style.setProperty("width","30px");
+    resultado.style.setProperty("height","30px");
+    resultado.style.setProperty("border-radius","100%");
+    resultado.style.setProperty("text-align","center");
 }
 
 btn.onclick = addOneBoludo;
